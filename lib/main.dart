64c05620baa6d1365006a2901a16ed8fd7fa3dfc
ananya0504 <ananyagupta0504@gmail.com>
2021-02-03 15:30:41 +0530

    Commented defualt transactions and built apk

--- lib/main.dart
+++ lib/main.dart
@@ -46,18 +46,18 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List <Transaction> _userTransactions = [
-    Transaction(
-    id: "t1",
-    title:"New Shoes",
-    amount: 69.99,
-    date: DateTime.now() 
-  ),
-  Transaction(
-    id: "t2",
-    title:"Weekly Groceries",
-    amount: 16.53,
-    date: DateTime.now() 
-  ),
+  //   Transaction(
+  //   id: "t1",
+  //   title:"New Shoes",
+  //   amount: 69.99,
+  //   date: DateTime.now() 
+  // ),
+  // Transaction(
+  //   id: "t2",
+  //   title:"Weekly Groceries",
+  //   amount: 16.53,
+  //   date: DateTime.now() 
+  // ),
   ];
 
   List <Transaction> get _recentTransactions { //getters do not have an argument list
